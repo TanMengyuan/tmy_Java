a2a99302e13b6e3c2dff32e0bd54467f415d8bbf
mengyuan.tan <tanmy1016@outlook.com>
2019-09-08 20:15:29 +0800

    20190908

--- src/Test1.java
+++ src/Test1.java
@@ -4,6 +4,12 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) {
 
+        int i = 1;
+        int j = i++;
+        if ((i>++j) && (i++==j)) {
+            j+=i;
+        }
+        System.out.println(j);
     }
 
 }
