47edbfd3e14a18c32aff98003a33a3e01660f005
mengyuan.tan <tanmy1016@outlook.com>
2019-09-11 22:26:04 +0800

    20190911

--- src/Test1.java
+++ src/Test1.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test1 {
-    public static void main(String[] args) {
-
+    public static void main ( String[] args) {
+        short a = 1;
+        long s = a;
+        System.out.println(s);
     }
 
-
+    public void Test1() {
+        System.out.println("sss");
+    }
 
 }
 
