619afae2a100b8baecf4a72763f354d4382b472f
mengyuan.tan <tanmy1016@outlook.com>
2019-09-15 22:39:01 +0800

    20190915

--- src/Test1.java
+++ src/Test1.java
@@ -1,15 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test1 {
     public static void main ( String[] args) {
-        short a = 1;
-        long s = a;
-        System.out.println(s);
-    }
-
-    public void Test1() {
-        System.out.println("sss");
+        String[] e = "1.12.1.0".split("\\.");
+        System.out.println(Arrays.toString(e));
     }
 
 }
