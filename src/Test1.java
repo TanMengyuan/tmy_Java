4190d3eed35c65867eea33505b7765469e04071d
mengyuan.tan <tanmy1016@outlook.com>
2019-09-21 09:47:49 +0800

    20190921

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Test1 {
-    public static void main ( String[] args) {
-        String[] e = "1.12.1.0".split("\\.");
-        System.out.println(Arrays.toString(e));
+    public static void main (String[] args) {
+        LinkedList l = new LinkedList();
+        l.add(12);
+        l.add(23);
+        System.out.println(l);
     }
 
 }
