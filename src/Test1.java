c3d4289a588a37a56b2f466b768b030aa4fbdbf3
mengyuan.tan <tanmy1016@outlook.com>
2019-10-07 08:16:29 +0800

    20191007

--- src/Test1.java
+++ src/Test1.java
@@ -5,10 +5,19 @@ import java.util.List;
 
 public class Test1 {
     public static void main (String[] args) {
-        LinkedList l = new LinkedList();
-        l.add(12);
-        l.add(23);
-        System.out.println(l);
+        int i = 0;
+        while (0 >= ++i) {
+            System.out.println("111");
+        }
+    }
+
+    synchronized void setA() throws Exception{
+        Thread.sleep(1000);
+        setB();
+    }
+
+    synchronized void setB() throws Exception{
+        Thread.sleep(1000);
     }
 
 }
